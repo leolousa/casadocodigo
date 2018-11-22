83d950b57af843a92d2f0f48cddc18735da161df
leolousa <leolousa@hotmail.com>
2018-11-22 09:52:45 -0200

    Implementando o WebSocket no cliente

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var app = require('./config/express')();
+var app = require('./config/express')();    // Importação do Express
+var http = require('http').Server(app);     // Importamos o http.Server para passar para o socket.io
+var io = require('socket.io')(http);        // Importamos o socket.io para rececber e enviar requisições em tempo real
 
-app.listen(3000, function(){
+app.set('io', io);
+
+http.listen(3000, function(){
   console.log('Servidor rodando na porta 3000!');
 });
